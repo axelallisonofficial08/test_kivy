d7d2d33e4013d2e6fed9b8ce5cd2ff3d7e2d9f86
axelallisonofficial08 <92298036+axelallisonofficial08@users.noreply.github.com>
2023-03-23 14:45:25 +0700

    Add files via upload
    update to the code

--- test_nextpage.py
+++ test_nextpage.py
@@ -14,24 +14,41 @@ class MyApp(App):
         # Create the main screen
         main_screen = Screen(name='main')
         layout = BoxLayout(orientation='vertical')
-        button1 = Button(text="Press Me 1", on_press=self.show_image)
-        button2 = Button(text="Press Me 2", on_press=self.read_rfid)
+        button1 = Button(text="Display Pic", on_press=self.show_image)
+        button2 = Button(text="Display 2 button", on_press=self.duplicate_card)
         layout.add_widget(button1)
         layout.add_widget(button2)
         main_screen.add_widget(layout)
         sm.add_widget(main_screen)
 
         # Create the RFID screen
-        rfid_screen = Screen(name='rfid')
+        duplicate_screen = Screen(name='Duplicate')
         layout = BoxLayout(orientation='vertical')
-        button1 = Button(text="Button 1")
-        button2 = Button(text="Button 2")
-        layout.add_widget(button1)
-        layout.add_widget(button2)
-        rfid_screen.add_widget(layout)
-        sm.add_widget(rfid_screen)
+        button_read_AC = Button(text="Read Access Card", on_press=self.read_card)
+        button_write_AC = Button(text="Write to new Access Card")
+        button_backToMain = Button(text="back", on_press=self.back_toMain)
+        layout.add_widget(button_read_AC)
+        layout.add_widget(button_write_AC)
+        layout.add_widget(button_backToMain)
+        duplicate_screen.add_widget(layout)
+        sm.add_widget(duplicate_screen)
+
+        #Page for reading access card
+        read_access_card_screen = Screen(name='Read_card')
+        layout = BoxLayout(orientation='vertical')
+        button_read = Button(text="Read Access Card")
+        button_back = Button(text="back", on_press=self.back_toMain)
+        layout.add_widget(button_read)
+        layout.add_widget(button_back)
+        read_access_card_screen.add_widget(layout)
+        sm.add_widget(read_access_card_screen)
+
 
         return sm
+
+    def back_toMain(self, instance):
+        # Switch to the RFID screen
+        self.root.current = 'main'
 
     def show_image(self, instance):
         # Create a popup window
@@ -40,9 +57,16 @@ class MyApp(App):
         popup.add_widget(image)
         popup.open()
 
-    def read_rfid(self, instance):
+    def duplicate_card(self, instance):
+        # Switch to the duplicate screen
+        self.root.current = 'Duplicate'
+
+
+    def read_card(self, instance):
         # Switch to the RFID screen
-        self.root.current = 'rfid'
+        self.root.current = 'Read_card'
+
+
 
         
 if __name__ == '__main__':
